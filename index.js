7c7d66899581b0f66fa91f638baebaea01ad4436
unknown <shchetinin.srg@yandex.ru>
2023-09-22 10:49:24 +0500

    fix validate

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const typeId = document.querySelector('.input_typeid')
-const utmCompaign = document.querySelector('.input_compaign')
-const cityId = document.querySelector('.input_cityid')
-const erid = document.querySelector('.input_erid')
-const result = document.querySelector('.result')
-const url = document.querySelector('.input_url')
-const validate = document.querySelector('.validate')
-const inputList = Array.from(document.getElementsByTagName("input"))
+const typeId = document.querySelector(".input_typeid");
+const utmCompaign = document.querySelector(".input_compaign");
+const cityId = document.querySelector(".input_cityid");
+const erid = document.querySelector(".input_erid");
+const result = document.querySelector(".result");
+const url = document.querySelector(".input_url");
+const validate = document.querySelector(".validate");
+const inputList = Array.from(document.getElementsByTagName("input"));
 
 function generateUtm() {
-  result.textContent = `${url.value}?utm_source=tg&utm_medium=cpc&utm_campaign=${utmCompaign.value}&utm_content=type_id:${typeId.value}|city_id:${cityId.value}&erid=${erid.value}`
-  validate.textContent = `${result.textContent.length} из 260`
-  if(result.textContent.length > 260) {
-    validate.style.color = "red"
+  result.textContent = `${url.value}?utm_source=tg&utm_medium=cpc&utm_campaign=${utmCompaign.value}&utm_content=type_id:${typeId.value}|city_id:${cityId.value}&erid=${erid.value}`;
+  validate.textContent = `${result.textContent.length} из 260`;
+  if (result.textContent.length > 260) {
+    validate.style.color = "red";
+    validate.textContent = `${result.textContent.length} из 260 слишком много`;
   }
 }
 
-inputList.forEach(i => {
-    i.addEventListener('input', generateUtm)
-    
+inputList.forEach((i) => {
+  i.addEventListener("input", generateUtm);
 });
-
-
